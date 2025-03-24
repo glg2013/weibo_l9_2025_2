38c6aeaa6247bb0578673de85882345ea7e088d7
glg2013 <57412960@qq.com>
2025-03-24 14:23:11 +0000

    查看用户列表

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()->count(50)->create();
+
+        $user = User::first();
+        $user->name = 'fengniancong';
+        $user->email = 'fengniancong@163.com';
+        $user->password = bcrypt('123456');
+        $user->save();
+
+        $user = User::find(2);
+        $user->name = 'honglang';
+        $user->email = 'honglang@163.com';
+        $user->password = bcrypt('123456');
+        $user->save();
+    }
+}
