aa557709c83b61356a9c2af282b3ec97255ba87e
glg2013 <57412960@qq.com>
2025-03-24 12:44:05 +0000

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -36,4 +36,39 @@ class UsersController extends Controller
         session()->flash('success', '欢迎，您将在这里凯一段新的旅程！');
         return redirect()->route('users.show', compact('user'));
     }
+
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $updateName = '';
+        $validateData = [
+            'password' => 'nullable|confirmed|min:6'
+        ];
+        if ($request->get('name') != $user->name) {
+            $updateName = $request->get('name');
+            $validateData['name'] = 'required|unique:users|max:50|min:3';
+        }
+
+        $this->validate($request, $validateData);
+
+        $data = [];
+        if ($updateName) {
+            $data['name'] = $updateName;
+        }
+        if ($request->get('password')) {
+            $data['password'] = bcrypt($request->get('password'));
+        }
+        if ($data) {
+            $user->update($data);
+        }
+
+        // 提示
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('users.show', compact('user'));
+    }
 }
