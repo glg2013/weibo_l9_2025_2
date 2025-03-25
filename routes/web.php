7df98100bcb15740313baa51d3da938456183ce0
glg2013 <57412960@qq.com>
2025-03-25 03:43:40 +0000

    发送找回密码链接

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PasswordController;
 use App\Http\Controllers\SessionsController;
 use App\Http\Controllers\StaticPagesController;
 use App\Http\Controllers\UsersController;
@@ -33,3 +34,9 @@ Route::delete('logout', [SessionsController::class, 'destroy'])->name('logout');
 // 激活
 Route::get('signup/confirm/{token}', [UsersController::class, 'confirmEmail'])->name('confirm_email');
 
+// 重置密码
+Route::get('password/reset', [PasswordController::class, 'showLinkRequestForm'])->name('password.request');
+Route::post('password/email', [PasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+
+Route::get('password/reset/{token}', [PasswordController::class, 'showResetForm'])->name('password.reset');
+Route::post('password/reset', [PasswordController::class, 'reset'])->name('password.update');
